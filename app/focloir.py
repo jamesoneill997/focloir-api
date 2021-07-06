470a19d4926dfaa5fa55d580dbba77221eb2301d
jamesoneill997 <jamesoneill997@gmail.com>
2021-07-06 19:13:00 +0100

    add get_translation and run selenium headless

--- app/focloir.py
+++ app/focloir.py
@@ -1,13 +1,17 @@
 from bs4 import BeautifulSoup
 
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+import selenium.webdriver.support.expected_conditions as ec
+
 
 
 def init():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_experimental_option("detach", True)  
-    driver = webdriver.Chrome(chrome_options=chrome_options) 
+    chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(options=chrome_options) 
    
     return driver
 
@@ -15,6 +19,8 @@ def search_term(driver, term):
     url = "https://www.focloir.ie/en/dictionary/ei/"
     formatted_term = format_term(term)
     driver.get(url+formatted_term)
+    WebDriverWait(driver, timeout=15).until(ec.presence_of_element_located((By.CLASS_NAME, "sense")))
+    print(get_translation(driver))
     return
 
 def format_term(term):
@@ -28,12 +34,16 @@ def format_term(term):
             formatted_string = word + "+"
         else:
             formatted_string += word
-            
+
     return formatted_string
+
+def get_translation(driver):
+    results = driver.find_element(By.CLASS_NAME, 'cit_translation')
+    return results.text
 
 
 def main():
     driver = init()
-    search_term(driver, 'game')
+    search_term(driver, 'run')
 if __name__ == '__main__':
     main()
