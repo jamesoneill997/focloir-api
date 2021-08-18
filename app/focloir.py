1a30a05e2b2615aaa9bbdfdb5bb9fea6f03e026b
jamesoneill997 <jamesoneill997@gmail.com>
2021-08-18 21:57:31 +0100

    add Focloir class

--- app/focloir.py
+++ app/focloir.py
@@ -3,43 +3,41 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 import selenium.webdriver.support.expected_conditions as ec
 
-def init():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_experimental_option("detach", True)
-    chrome_options.add_argument('--headless')
-    driver = webdriver.Chrome(options=chrome_options) 
-   
-    return driver
+class Focloir:
+    def __init__(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("detach", True)
+        chrome_options.add_argument('--headless')
+        driver = webdriver.Chrome(options=chrome_options) 
+        self.driver = driver
 
-def search_term(driver, term):
-    url = "https://www.focloir.ie/en/dictionary/ei/"
-    formatted_term = format_term(term)
-    driver.get(url+formatted_term)
-    WebDriverWait(driver, timeout=15).until(ec.presence_of_element_located((By.CLASS_NAME, "sense")))
-    print(get_translation(driver))
-    return
+    def search_term(self, term):
+        driver = self.driver
+        url = "https://www.focloir.ie/en/dictionary/ei/"
+        formatted_term = self.format_term(term)
+        driver.get(url+formatted_term)
+        WebDriverWait(driver, timeout=15).until(ec.presence_of_element_located((By.CLASS_NAME, "sense")))
+        print(self.get_translation())
+        return
 
-def format_term(term):
-    formatted_string = ""
-    words = term.split(" ")
-    #if only 1 word, formatting not needed
-    if len(words) == 1:
-        return term
-    for word in words:
-        if not words.index(word) == len(words) - 1:  #if it isn't the last word
-            formatted_string = word + "+"
+    def format_term(self, term):
+        formatted_string = ""
+        words = term.split(" ")
+        #if only 1 word, formatting not needed
+        if len(words) == 1:
+            return term
+        for word in words:
+            if not words.index(word) == len(words) - 1:  #if it isn't the last word
+                formatted_string = word + "+"
+            else:
+                formatted_string += word
+
+        return formatted_string
+
+    def get_translation(self):
+        driver = self.driver
+        results = driver.find_element(By.CLASS_NAME, 'cit_translation').text
+        if "masc" in results.split(" ")[1] or "fem" in results.split(" ")[1]:
+            return results.split(" ")[0]
         else:
-            formatted_string += word
-
-    return formatted_string
-
-def get_translation(driver):
-    results = driver.find_element(By.CLASS_NAME, 'cit_translation')
-    return results.text
-
-
-def main():
-    driver = init()
-    search_term(driver, 'run')
-if __name__ == '__main__':
-    main()
+            return results
